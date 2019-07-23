90cdb387467bbf1367861aab0473796baa802ed7
pzy <1012839072@qq.com>
2019-07-23 16:06:50 +0800

    first

---
+++ src/pages/page-turning/page-turning.js
@@ -0,0 +1,35 @@
+// pages/page-turning/page-turning.js
+const app = getApp()
+Page({
+  data: {
+    windowWidth: app.globalData.windowWidth, //单位px
+    windowHeight: app.globalData.windowHeight, //单位px
+    diff: 500, //动态相差px
+    rotate: 30, //deg
+    imageList: [
+      '',//第一张不显示，设置为空
+      'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3294683064,1020587522&fm=26&gp=0.jpg',
+      'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=309563235,1645727097&fm=26&gp=0.jpg',
+      'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2651627281,3056935767&fm=26&gp=0.jpg',
+      'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1731302165,2444560272&fm=26&gp=0.jpg',
+    ],
+    currentPage: 1,
+    turnPage: 0,
+  },
+  onLoad: function(options) {
+
+  },
+  bindTurn() {
+    const that = this
+    that.setData({
+      turnPage: that.data.turnPage + 1,
+    })
+    setTimeout(()=>{
+      that.setData({
+        currentPage: that.data.currentPage+1,
+      })
+    },1000)
+
+  }
+
+})
