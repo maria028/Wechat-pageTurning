ca8fe1484ee8df4c2a46236220e9148b61538610
pzy <1012839072@qq.com>
2019-07-24 10:28:55 +0800

    前后翻页

--- src/pages/page-turning/page-turning.js
+++ src/pages/page-turning/page-turning.js
@@ -7,7 +7,7 @@ Page({
     diff: 500, //动态相差px
     rotate: 30, //deg
     imageList: [
-      '',//第一张不显示，设置为空
+      '', //第一张不显示，设置为空
       'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3294683064,1020587522&fm=26&gp=0.jpg',
       'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=309563235,1645727097&fm=26&gp=0.jpg',
       'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2651627281,3056935767&fm=26&gp=0.jpg',
@@ -19,17 +19,28 @@ Page({
   onLoad: function(options) {
 
   },
-  bindTurn() {
+  bindTurn(e) {
     const that = this
-    that.setData({
-      turnPage: that.data.turnPage + 1,
-    })
-    setTimeout(()=>{
+    const clientX = e.touches[0].clientX
+
+    if (clientX > that.data.windowWidth / 2) {
       that.setData({
-        currentPage: that.data.currentPage+1,
+        turnPage: that.data.turnPage + 1,
       })
-    },1000)
-
+      setTimeout(() => {
+        that.setData({
+          currentPage: that.data.currentPage + 1,
+        })
+      }, 1000)
+    }else{
+      that.setData({
+        currentPage: that.data.currentPage - 1,
+      })
+      setTimeout(() => {
+        that.setData({
+          turnPage: that.data.turnPage - 1,
+        })
+      }, 500)
+    }
   }
-
 })
